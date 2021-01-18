bbb6de1649a86a99df17b3e5cad608fec858d580
Shira <shirafeldinger@gmail.com>
2021-01-18 22:18:20 +0200

    set new component for gameBoard

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, } from 'react-native';
-import { Provider, useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import GameBoard from './components/gameBoard';
 
-type SimonState = {
+export type SimonState = {
   simonsColors: Array<string>;
   score: number;
   userColors: Array<string>;
@@ -58,100 +58,9 @@ export const AppWrapper = () => {
 }
 
 const App = () => {
-  const userColors = useSelector<SimonState>(state => state.userColors) as Array<string>;
-  const simonsColors = useSelector<SimonState>(state => state.simonsColors) as Array<string>;
-  const score = useSelector<SimonState>(state => state.score) as number;
-  const dispatch = useDispatch();
-  const colors = ['red', 'blue', 'green', 'yellow'];
-
-  const manageSimonchoice = () => {
-    let newSimonsColors: string[] = [...simonsColors]
-    let chosenColor = colors[Math.floor(Math.random() * 3)];
-    newSimonsColors.push(chosenColor);
-    console.log('going to dispatch', [...newSimonsColors]);
-
-    dispatch({ type: 'SET_SIMON_COLORS', simonsColors: [...newSimonsColors] });
-  }
-  const manageUserChoice = (chosenColor: string) => {
-    let newUserColors: string[] = [...userColors]
-    newUserColors.push(chosenColor)
-    dispatch({ type: 'SET_USER_COLORS', userColors: [...newUserColors] });
-  };
-
-  const checkUserSelection = () => {
-    if (JSON.stringify(userColors) == JSON.stringify(simonsColors)) {
-      dispatch([{ type: 'SET_SCORE' }, { type: 'SET_USER_COLORS', userColors: [] }]);
-      manageSimonchoice();
-    } else {
-      alert('You lose');
-      dispatch({ type: 'RESET' });
-    }
-  };
-  console.log(userColors, 'user');
-  console.log(simonsColors, 'simon');
-
-  const checkTurn = () => {
-    if (userColors.length == simonsColors.length && simonsColors.length != 0) {
-      checkUserSelection();
-    }
-  };
-  checkTurn();
-  /// listen on state.simonColors using useEffect. on every state change fire a function that goes over 
-  // the array of colors one by one whith timeout between one another and triggers an animation state for a matching color element.
-  const isActive = false;
-
   return (
-    <View style={styles.container}>
-      <View style={styles.boradContainer}>
-        <View>
-          <TouchableOpacity style={[styles.touchableOpacityStyle, { backgroundColor: 'blue', borderTopLeftRadius: 100 }]} onPress={() => { manageUserChoice('blue') }} />
-          <TouchableOpacity style={[styles.touchableOpacityStyle, { backgroundColor: 'green', borderBottomLeftRadius: 100 }]} onPress={() => { manageUserChoice('green') }} />
-        </View>
-        <View>
-          <TouchableOpacity style={[styles.touchableOpacityStyle, { backgroundColor: 'yellow', opacity: isActive ? 1 : 0.5, borderTopRightRadius: 100 }]} onPress={() => { manageUserChoice('yellow') }} />
-          <TouchableOpacity style={[styles.touchableOpacityStyle, { backgroundColor: 'red', borderBottomRightRadius: 100 }]} onPress={() => { manageUserChoice('red') }} />
-        </View>
-        <TouchableOpacity style={styles.centerBottom} onPress={() => manageSimonchoice()} >
-          <Text style={{ fontSize: 20 }}>Start</Text>
-        </TouchableOpacity>
-      </View>
-      <Text style={{ flex: 1, alignSelf: 'center', fontSize: 20 }}>Current Scrore:{score}</Text>
-    </View>
+    <GameBoard />
   )
 };
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  boradContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-    margin: '5%',
-    position: 'relative'
-  },
-  touchableOpacityStyle: {
-    height: 105,
-    width: 105,
-    borderColor: 'black',
-    borderWidth: 10
-  },
-  centerBottom: {
-    position: 'absolute',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    borderRadius: 50,
-    borderWidth: 15,
-    borderColor: 'black',
-    height: 100,
-    width: 100
-  }
-});
 
 export default App;
